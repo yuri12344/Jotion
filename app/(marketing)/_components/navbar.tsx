4eaa9c4f4fd93bc2b4be5e65dce090ba09c60a1a
Yuri Caetano <yuuri.caetano@gmail.com>
2023-10-13 10:53:17 -0300

    feat: add custom react hook useScrollTop

---
+++ app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+import { Logo } from "./logo";
+
+const Navbar = () => {
+  const scrolled = useScrollTop();
+
+  return (
+    <div
+      className={cn(
+        "z-50 bg-backgroud fixed top-0 flex items-center w-full p-6",
+        scrolled && "border-b shadow-md",
+      )}
+    >
+      <Logo />
+    </div>
+  );
+};
+
+export default Navbar;
