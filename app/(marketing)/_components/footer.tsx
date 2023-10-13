17734bc2e7c73d0b45e8e27689cf5936baab2656
Yuri Caetano <yuuri.caetano@gmail.com>
2023-10-13 10:16:06 -0300

    adjustments in the footer component

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -5,7 +5,6 @@ export const Footer = () => {
   return (
     <div className="flex items-center w-full p-6 bg-back z-50">
       <Logo />
-
       <div className="flex w-full justify-between md:justify-end text-muted-foreground gap-x-2">
         <Button variant="ghost" size="sm">
           Privacy Policy
