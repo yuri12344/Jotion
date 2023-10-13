607c940d2a84c3b2a47a88bc8ae22ba0507c6efa
Yuri Caetano <yuuri.caetano@gmail.com>
2023-10-12 23:57:05 -0300

    feat: create footer

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
 import { Logo } from "./logo";
 
 export const Footer = () => {
   return (
-    <div className="flex items-center w-full p-6 bg-background z-50">
-      <Logo></Logo>
+    <div className="flex items-center w-full p-6 bg-back z-50">
+      <Logo />
+
+      <div className="flex w-full justify-between md:justify-end text-muted-foreground gap-x-2">
+        <Button variant="ghost" size="sm">
+          Privacy Policy
+        </Button>
+        <Button variant="ghost" size="sm">
+          Terms & Conditions
+        </Button>
+      </div>
     </div>
   );
 };
